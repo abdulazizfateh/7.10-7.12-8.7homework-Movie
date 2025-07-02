6ea2e2c8338e82d7cc87db4276706374b2c83dfb
abdulazizfateh <eabdulaziz36@gmail.com>
2025-07-02 14:43:04 +0500

    Fixed - Suspense Loading

--- src/utils/utils.tsx
+++ src/utils/utils.tsx
@@ -10,10 +10,11 @@ interface Props {
 }
 
 export const Loading = () => {
-
+    const storedTheme = localStorage.getItem("theme");
+    const theme: boolean = storedTheme ? JSON.parse(storedTheme) : false;
     return (
         <>
-            <div className="logo-loader">
+            <div className={`${theme ? "bg-bg-light-900" : "bg-bg-dark-900"} logo-loader`}>
                 <img className="icon icon1" src={Logo1} alt="Logo" />
                 <img className="icon icon2" src={Logo2} alt="Logo" />
                 <img className="icon icon3" src={Logo3} alt="Logo" />
