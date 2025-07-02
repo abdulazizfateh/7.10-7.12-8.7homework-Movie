44b2c6428e119dbe72b347f61ef9498d5478a331
abdulazizfateh <eabdulaziz36@gmail.com>
2025-07-03 02:34:43 +0500

    Fixed - Deployment Error

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { IMAGE_URL } from '@/const';
 // Swiper
 import { Swiper, SwiperSlide } from 'swiper/react';
